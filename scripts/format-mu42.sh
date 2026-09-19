2dc0e2c5935f49e1d6adb05050796c1c265f4116
Kiswono Prayogo <kiswono@gmail.com>
2026-09-19 22:48:41 +0800

    fix(pipeline): keep M-U42 formatter self-contained

--- scripts/format-mu42.sh
+++ scripts/format-mu42.sh
@@ -5,5 +5,4 @@ gofmt -w \
   hat/hatPipeline/connector_checkpoint.go \
   hat/hatPipeline/connector_lifecycle.go \
   hat/hatPipeline/m_u42_connector_checkpoint_test.go \
-  hat/hatPipeline/m_u42_connector_checkpoint_benchmark_test.go \
-  scripts/commit-mu42.go
+  hat/hatPipeline/m_u42_connector_checkpoint_benchmark_test.go
